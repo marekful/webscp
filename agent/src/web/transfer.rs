99f8a39fa3eacd5baa265e6e694e806420e0e4d2
Marcell Fülöp <marekful@domainloop.net>
2023-04-04 10:16:37 +0000

    refactor(agent): don't send response body for transfer delete request

--- agent/src/web/transfer.rs
+++ agent/src/web/transfer.rs
@@ -1,19 +1,10 @@
-use crate::constants::DEFAULTS;
-use rocket::{
-    http::Status,
-    serde::{json::Json, Serialize},
-};
+use rocket::http::Status;
 use tokio::process::Command;
 
-#[derive(Serialize, Debug)]
-pub struct CancelTransferResponse {
-    code: i32,
-    success: bool,
-    message: Option<String>,
-}
+use crate::constants::DEFAULTS;
 
 #[delete("/transfers/<transfer_id>")]
-pub async fn cancel_transfer(transfer_id: &str) -> (Status, Json<CancelTransferResponse>) {
+pub async fn cancel_transfer(transfer_id: &str) -> Status {
     // create argument list for uploader script
     let mut script_args: Vec<&str> = Vec::new();
     script_args.push(DEFAULTS.cancel_transfer_script_path);
@@ -24,36 +15,18 @@ pub async fn cancel_transfer(transfer_id: &str) -> (Status, Json<CancelTransferR
     let child = cmd.args(script_args).spawn();
     let result = child.unwrap().wait().await;
 
+    // respond with error on command error
     if result.is_err() {
-        return (
-            Status::InternalServerError,
-            Json(CancelTransferResponse {
-                code: 119,
-                success: false,
-                message: None,
-            }),
-        );
+        return Status::InternalServerError;
     }
 
+    // respond with not found on non-zero exit code
     let result = result.unwrap();
     let code = result.code().unwrap();
     if code != 0 {
-        return (
-            Status::NotFound,
-            Json(CancelTransferResponse {
-                code: 120,
-                success: false,
-                message: Some(format!("code: {code}")),
-            }),
-        );
+        return Status::NotFound;
     }
 
-    (
-        Status::Ok,
-        Json(CancelTransferResponse {
-            code: 0,
-            success: true,
-            message: None,
-        }),
-    )
+    // success response
+    Status::Ok
 }
