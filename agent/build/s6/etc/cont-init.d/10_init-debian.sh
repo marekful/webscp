c28005ef3b9e95d6b5eedbfc34fe94f42cfbeb83
Marcell Fülöp <marekful@domainloop.net>
2023-04-19 14:21:16 +0000

    build(agent): Create separate Debian and Alpine based release builds

---
+++ agent/build/s6/etc/cont-init.d/10_init-debian.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/with-contenv bash
+
+if grep ^ID=alpine /etc/*release* > /dev/null; then
+  exit 0
+fi
+
+echo "Debian init"
+
+mkdir -p /run/s6/container_environment /run/sshd
+
+if grep agent /etc/passwd >/dev/null; then
+  echo "User 'agent' exists"
+else
+  echo "Adding user 'agent'"
+  useradd -m -s /bin/bash agent
+fi
+
+mkdir -p /home/agent/.tmp-data
+chown -R agent:agent /home/agent
+
+chown agent:agent /etc/scripts/uploader.sh /etc/scripts/cancel-transfer.sh \
+                  /etc/scripts/generate-key-pair.sh /etc/scripts/revoke-key-pair.sh
+chmod u+x /etc/scripts/uploader.sh /etc/scripts/cancel-transfer.sh \
+          /etc/scripts/generate-key-pair.sh /etc/scripts/revoke-key-pair.sh
+
+if [ -f /home/agent/.ssh/id_rsa ];then
+  echo "SSH keys exist"
+  exit 0
+fi
+
+echo "Generating SSH keys"
+mkdir -p /home/agent/.ssh && \
+    ssh-keygen -q -t rsa -b 4096 -f /home/agent/.ssh/id_rsa -N "" && \
+    chown -R agent:agent /home/agent/.ssh && \
+    mkdir -p /etc/ssh/host-keys && \
+    ssh-keygen -f /etc/ssh/host-keys/ssh_host_ecdsa_key -N '' -t ecdsa
