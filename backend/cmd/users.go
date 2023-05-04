marekful/webscp

commit fc48ea62640231da3b5b6a7ef5444c54f6fd13d4

fix(backend): Avoid casting integer to smaller size

backend/cmd/users.go
@@ -52,12 +52,12 @@ func printUsers(usrs []*users.User) {
 	w.Flush()
 }
 
-func parseUsernameOrID(arg string) (username string, id uint) {
+func parseUsernameOrID(arg string) (username string, id uint64) {
 	id64, err := strconv.ParseUint(arg, 10, 64)
 	if err != nil {
 		return arg, 0
 	}
-	return "", uint(id64)
+	return "", id64
 }
 
 func addUserFlags(flags *pflag.FlagSet) {

backend/http/agents.go
@@ -122,7 +122,7 @@ func withAgentUser(fn handleFunc) handleFunc {
 			return http.StatusUnauthorized, nil
 		}
 
-		user, dErr := d.store.Users.Get(d.server.Root, uint(id64))
+		user, dErr := d.store.Users.Get(d.server.Root, id64)
 		if dErr != nil {
 			return http.StatusUnauthorized, nil
 		}

commit 40188f3f9f639d2f2a1c118f744c5a1bfcc479ea

fix(backend): Fix parsing user ID to uint32
The error was introduced in fc48ea62640231da3b5b6a7ef5444c54f6fd13d4
which addressed the CodeQL finding that casting int64 to int32 was bad.

backend/cmd/users.go
@@ -52,12 +52,12 @@ func printUsers(usrs []*users.User) {
 	w.Flush()
 }
 
-func parseUsernameOrID(arg string) (username string, id uint64) {
-	id64, err := strconv.ParseUint(arg, 10, 64)
+func parseUsernameOrID(arg string) (username string, id uint) {
+	rawID, err := strconv.Atoi(arg)
 	if err != nil {
 		return arg, 0
 	}
-	return "", id64
+	return "", uint(rawID)
 }
 
 func addUserFlags(flags *pflag.FlagSet) {

backend/http/agents.go
@@ -116,13 +116,17 @@ func withAgentUser(fn handleFunc) handleFunc {
 
 		// Fetch the user referred to in query params
 		vars := mux.Vars(r)
-		userID := vars["user_id"]
-		id64, err := strconv.ParseUint(userID, 10, 64)
+		userID, set := vars["user_id"]
+		if !set {
+			return http.StatusUnauthorized, nil
+		}
+
+		rawID, err := strconv.Atoi(userID)
 		if err != nil {
 			return http.StatusUnauthorized, nil
 		}
 
-		user, dErr := d.store.Users.Get(d.server.Root, id64)
+		user, dErr := d.store.Users.Get(d.server.Root, uint(rawID))
 		if dErr != nil {
 			return http.StatusUnauthorized, nil
 		}