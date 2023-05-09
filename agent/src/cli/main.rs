5b62b89ceabab7060916a956c21d0ca5f76e1012
Marcell Fülöp <marekful@domainloop.net>
2023-05-09 06:29:02 +0000

    ci: dummy commit to trigger rustfmt and clippy

--- agent/src/cli/main.rs
+++ agent/src/cli/main.rs
@@ -54,7 +54,6 @@ fn main() {
         COMMAND_GET_REMOTE_VERSION => exec = Some(command_get_remote_version),
         COMMAND_GET_REMOTE_RESOURCE => exec = Some(command_get_remote_resource),
         COMMAND_REMOTE_BEFORE_COPY => exec = Some(command_remote_before_copy),
-        /*COMMAND_REMOTE_DO_COPY => exec = Some(command_remote_do_copy),*/
         COMMAND_GET_REMOTE_USER => exec = Some(command_get_remote_user),
         COMMAND_GET_TOKEN_USER => exec = Some(command_get_token_user),
         COMMAND_PING => exec = Some(command_ping),
